4e1b1d0b9f6752e1a3f23596fac0ba3acf446a28
unknown <lsw@cdslst.kr>
2025-05-22 12:03:44 +0900

    asdasd

--- Rasberry/enc_plant_pid.go
+++ Rasberry/enc_plant_pid.go
@@ -62,47 +62,60 @@ func main() {
 	for angle := range angleChan {
 		loopStart := time.Now()
 
+		serial_start := time.Now()
+		// 시리얼 읽기는 readSerialLoop에서 하므로 패스
+		fmt.Printf("1. 시리얼 통신 시간: %v\n", time.Since(serial_start))
+
+		fmt.Println("받은 데이터 (Angle):", angle)
+
 		quantized_angle := math.Round((1 / r) * angle)
 		encode := []int64{int64(quantized_angle)}
-		encoder.Encode(encode, pt_angle)
 
+		encode_start := time.Now()
+		encoder.Encode(encode, pt_angle)
 		ct_angle, _ := encryptor.EncryptNew(pt_angle)
 		serialized_angle, err := ct_angle.MarshalBinary()
 		if err != nil {
 			fmt.Println("암호문 직렬화 실패:", err)
 			continue
 		}
+		fmt.Printf("2. 암호화 및 데이터 변환 시간: %v\n", time.Since(encode_start))
 
+		com_start := time.Now()
 		_, err = conn.Write(serialized_angle)
 		if err != nil {
 			fmt.Println("TCP 전송 실패:", err)
 			break
 		}
-
 		response, err := com_utils.ReadFullData(conn, 131406)
 		if err != nil {
 			fmt.Println("응답 수신 실패:", err)
 			break
 		}
+		fmt.Printf("3. 암호 데이터의 TCP 송수신 시간: %v\n", time.Since(com_start))
 
+		dec_start := time.Now()
 		ct_U := rlwe.NewCiphertext(params, params.MaxLevel())
 		err = ct_U.UnmarshalBinary(response)
 		if err != nil {
 			fmt.Println("응답 역직렬화 실패:", err)
 			break
 		}
-
 		dec_U := decryptor.DecryptNew(ct_U)
 		decoded := make([]int64, 1)
 		encoder.Decode(dec_U, decoded)
 		pwm_u := float64(decoded[0]) * r
+		fmt.Printf("4. 복호화 및 데이터 변환 시간: %v\n", time.Since(dec_start))
 
 		fmt.Println("제어 결과:", pwm_u)
+
+		serial_2_start := time.Now()
 		_, err = port.Write([]byte(fmt.Sprintf("%d,%d\n", int(pwm_u), 1)))
 		if err != nil {
 			fmt.Println("시리얼 전송 실패:", err)
 			break
 		}
+		fmt.Printf("5. 시리얼 송신 시간: %v\n", time.Since(serial_2_start))
 
 		fmt.Printf("총 루프 처리 시간: %v\n", time.Since(loopStart))
 		fmt.Println("-----------------------------------")
