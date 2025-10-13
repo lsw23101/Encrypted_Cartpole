68a1371fc3081d25ef38449516565ff7214da81b
Your Name <lsw231@cdslst.com>
2025-10-13 13:53:12 +0900

    N12

--- PC/controller_rgsw.go
+++ PC/controller_rgsw.go
@@ -18,8 +18,8 @@ import (
 )
 
 const (
-	addr = "192.168.0.20:8080" // 서버 바인딩 주소
-	// addr = "192.168.0.115:8080" // 서버 바인딩 주소
+	// addr = "192.168.0.20:8080" // 서버 바인딩 주소
+	addr = "192.168.0.115:8080" // 서버 바인딩 주소
 	// addr     = "127.0.0.1:9000" // 컨트롤러 주소
 	numIters = 500 // 0 means infinite loop
 	period   = 0 * time.Millisecond
@@ -30,7 +30,7 @@ func ms(d time.Duration) float64 { return float64(d) / 1e6 }
 func main() {
 	// ======== Parameters (저장 당시와 동일) ========
 	params, _ := rlwe.NewParametersFromLiteral(rlwe.ParametersLiteral{
-		LogN: 11, LogQ: []int{56}, LogP: []int{51}, NTTFlag: true,
+		LogN: 12, LogQ: []int{56}, LogP: []int{51}, NTTFlag: true,
 	})
 	ringQ := params.RingQ()
 
