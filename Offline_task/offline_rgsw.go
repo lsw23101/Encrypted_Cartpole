e4d543af333c98d996f77ad4b7c4a92dd2868fe3
Your Name <lsw231@cdslst.com>
2025-10-13 16:30:34 +0900

    1013 final

--- Offline_task/offline_rgsw.go
+++ Offline_task/offline_rgsw.go
@@ -74,7 +74,7 @@ func main() {
 	// ================= 2) Quantization parameters =================
 	s := 1 / 10.0
 	L := 1 / 10000.0
-	r := 1 / 1000.0
+	r := 1 / 10000.0
 	fmt.Printf("Scaling parameters 1/L: %v, 1/s: %v, 1/r: %v\n", 1/L, 1/s, 1/r)
 
 	// ================= 3) Rings / aux =================
